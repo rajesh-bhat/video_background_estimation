4b9dbe3c68b8b6dde99c723a8d0aec788fb5f2a3
rajesh-bhat <rajeshbht19@gmail.com>
2023-01-17 22:34:44 +0000

    formatting and linting added

--- video_background_estimation/EstimateBackground.py
+++ video_background_estimation/EstimateBackground.py
@@ -9,7 +9,7 @@ class BackgroundEstimation:
         self.num_frames = self.cap.get(cv2.CAP_PROP_FRAME_COUNT)
         self.frame_fraction = frame_fraction
         self.frame_fraction_num = int(self.num_frames * self.frame_fraction)
-    
+
     def _mean_image(self, selected_frames):
         mean_frame = np.mean(selected_frames, axis=0).astype(dtype=np.uint8)
         return mean_frame
@@ -19,7 +19,7 @@ class BackgroundEstimation:
         return median_frame
     
     def _select_frames(self):
-
+        
         # Randomly select n frames
         frame_ids = self.num_frames * np.random.uniform(size = self.frame_fraction_num)
 
